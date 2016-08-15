fa4bf41227351ce6d8adbd57446bdf2bc51a3985
TimingLib <shuaishuaiweiba@163.com>
2016-08-15 15:11:52 +0800

    revert

--- polls/urls.py
+++ polls/urls.py
@@ -2,16 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     url(r'^$', views.index, name='index'),
-    #   url(r'^(?P<question_id>[0-9]+)/detail/$', views.detail, name='detail'),
-    #	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    #	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/detail/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
-=======
-	url(r'^$',views.index,name='index'),
-	url(r'^(?P<question_id>[0-9]+)/detail/$', views.detail, name='detail'),
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-	]
->>>>>>> parent of be7af50... Format the string
